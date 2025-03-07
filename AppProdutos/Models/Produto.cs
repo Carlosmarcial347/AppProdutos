784f2f0b4e86a2ed131927f5497e0c133cc5dde7
mauromarcial <mpaulamarcial@gmail.com>
2025-03-07 18:11:21 -0300

    Add models e troca de tela inicial

--- AppProdutos/Models/Produto.cs
+++ AppProdutos/Models/Produto.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace AppProdutos.Models
 {
-    internal class Produto
+    public class Produto
     {
+        [ PrimaryKey, AutoIncrement]
+
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public double Quantidade { get; set; }
+
+        public double Preco { get; set; }
     }
 }
