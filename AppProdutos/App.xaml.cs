4b280a87bbc0c14bca2a21cb8a7d615150e65a8a
mauromarcial <mpaulamarcial@gmail.com>
2025-03-07 19:49:11 -0300

    Inset feito

--- AppProdutos/App.xaml.cs
+++ AppProdutos/App.xaml.cs
@@ -1,7 +1,33 @@
-﻿namespace AppProdutos
+﻿
+
+
+using AppProdutos.Helpers;
+
+namespace AppProdutos
 {
     public partial class App : Application
     {
+
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db 
+        {
+            get 
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3" );
+
+                    _db = new SQLiteDatabaseHelper(".... db3");
+                }
+
+                return _db; 
+            } 
+        }
+
         public App()
         {
             InitializeComponent();
--- AppProdutos/Views/NovoProduto.xaml.cs
+++ AppProdutos/Views/NovoProduto.xaml.cs
@@ -1,9 +1,34 @@
+using AppProdutos.Models;
+
 namespace AppProdutos.Views;
 
 public partial class NovoProduto : ContentPage
 {
 	public NovoProduto()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+
+            };
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso", "Registro Inserido", "Ok");
+
+		} catch(Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+
+		}
+
+    }
 }
